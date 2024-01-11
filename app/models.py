71890b6b9f9cc8dda2bc33faa169b74f44243ba1
prasantagith <prasantasethi2002gmail.com>
2024-01-11 18:26:01 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django import forms
+# Create your models here.
+from django.core import validators
+
+def check_for_a(value):
+    if value.lower()[0]=='p':
+        raise forms.ValidationError('invalid')
+class Student(models.Model):
+    Sname=models.CharField(max_length=100,validators=[check_for_a])
+    Sage=models.IntegerField()
+    Smobile=models.CharField(max_length=10,validators=[validators.RegexValidator('[6-9]\d{9}')])
+    def __str__(self):
+        return  self.Sname
---
+++ app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from app.forms import *
+from django.http import HttpResponse
+def create_student(request):
+    ESFO=StudentForm()
+    d={'ESFO':ESFO}
+    if request.method=='POST':
+        SDFO=StudentForm(request.POST)
+        if SDFO.is_valid():
+            SDFO.save()
+            return HttpResponse('create_stu_complete')
+        else:
+            return HttpResponse('error')
+    
+    return render(request,'create_student.html',d)
